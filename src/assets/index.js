52eb081ef9eb485283576774e9fc96ed15a77e4a
hulanyu <hulanyu@ppctech.com.cn>
2019-02-27 10:24:43 +0800

    test

--- src/assets/index.js
+++
@@ -1,7 +0,0 @@
-import util from './util.js'
-import devTools from './devTools.js'
-import viewUtil from './viewUtil.js'
-import upload from './upload.js'
-import dateFormate from './dateFormate.js'
-
-export { util, devTools, viewUtil, upload, dateFormate }
